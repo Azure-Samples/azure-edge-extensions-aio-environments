f5ce164ebc3fb54eb8a0e1ff1dc6814ee62f57a7
Marcel Bindseil <marcelbindseil@gmail.com>
2023-11-15 11:47:03 +0100

    renaming RG

--- scripts/vm.sh
+++ scripts/vm.sh
@@ -4,7 +4,7 @@ az deployment sub create \
    --name arc-footprint-deployment \
    --location $LOCATION \
    --template-file ./main-image.bicep \
-   --parameters   resourceGroupName="rg-vm-vhdx-arc-footprint-new" \
+   --parameters   resourceGroupName="rg-vm-vhdx-arc-footprint" \
                   galleryName="igarcfootprint" \
                   imageDefinitionName="id-arc-footprint" \
                   imageVersion="latest"
