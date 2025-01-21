479d7873a7bab4b68025ccebb62551947e8c5cca
Marcel Bindseil <marcelbindseil@gmail.com>
2025-01-21 14:30:33 +0100

    back to prev
    Signed-off-by: Marcel Bindseil <marcelbindseil@gmail.com>

--- vm.sh
+++ vm.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 az vm extension set \
-            --resource-group rg-arc-exec \
-            --vm-name arc-exec \
+            --resource-group rg-arc-vmnext \
+            --vm-name arc-vmnext \
             --name customScript \
             --publisher Microsoft.Azure.Extensions \
             --force-update \
             --protected-settings "{\"commandToExecute\": \" \
                 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml && \
-                notReadyNodes=\$(kubectl get nodes | grep NotReady | awk '{print \$1}' 2>/dev/null) && \
-                if [ -n \"\"\$notReadyNotes\"\" ]; then kubectl delete node \"\"\$notReadyNotes\"\" fi && \
-                kubectl delete node \$(kubectl get nodes | grep NotReady | awk '{print \$1}') && \
+                notReadyNotes=\$(kubectl get nodes | grep NotReady | awk '{print \$1}' 2>/dev/null) && \
+                len=\${#notReadyNotes} && \
+                echo \"\"not ready\$len\"\" && \
+                if [[ -z \$notReadyNotes ]]; then echo \"\"xxxx\"\" && kubectl delete node \"\"\$notReadyNotes\"\"; fi && \
                 kubectl config use-context default && \
                 kubectl get pods -A -v6 \
             \"}"
